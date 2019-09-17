41018cbe58de4e838c4fe39d79cdd605a599ddbf
saadmohammed <jmsaad46@gmail.com>
2019-09-17 10:30:20 +0530

    initial commit

--- app/src/main/java/com/example/staffprofile/SplashActivity.java
+++ app/src/main/java/com/example/staffprofile/SplashActivity.java
@@ -1,16 +1,76 @@
 package com.example.staffprofile;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.os.Handler;
+import android.provider.Settings;
 
 public class SplashActivity extends AppCompatActivity {
+
+    private Handler handler;
+
+    private boolean haveNetworkConnected() {
+
+        boolean haveConnectedWifi = false;
+        boolean haveConnectedMobile = false;
+
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo[] infos = connectivityManager.getAllNetworkInfo();
+
+        for (NetworkInfo ni : infos) {
+            if (ni.getTypeName().equalsIgnoreCase("WIFI"))
+                if (ni.isConnected())
+                    haveConnectedWifi = true;
+            if (ni.getTypeName().equalsIgnoreCase("MOBILE"))
+                if (ni.isConnected())
+                    haveConnectedMobile = true;
+
+        }
+        return  haveConnectedWifi || haveConnectedMobile;
+    }
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        
+        setContentView(R.layout.activity_splash);
+
+        if(haveNetworkConnected()) {
+            handler = new Handler();
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(i);
+                    finish();
+                }
+            }, 3000);
+        }
+        else{
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("No Internet Connection");
+            builder.setMessage("You are offline.\n Please Check your Internet Connection");
+            builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    startActivity(new Intent(Settings.ACTION_WIRELESS_SETTINGS));
+                }
+            }).setCancelable(false);
+
+            builder.create().show();
+        }
+
+
 
     }
 }
