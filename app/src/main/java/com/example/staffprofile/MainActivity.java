8f15bb1d67b4e716e16c6a3968c0a31de98e782e
saadmohammed <jmsaad46@gmail.com>
2019-09-18 09:06:17 +0530

    initial commit

--- app/src/main/java/com/example/staffprofile/MainActivity.java
+++ app/src/main/java/com/example/staffprofile/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.staffprofile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //Font
+        CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
+                .setDefaultFontPath("fonts/small.otf")
+                .setFontAttrId(R.attr.fontPath)
+                .build());
+
         setContentView(R.layout.activity_main);
     }
 }
